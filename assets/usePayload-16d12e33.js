8fd127d62807316a9c775cb83e1155deca33739f
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2023-03-01 17:43:56 +0100

    :tada: update

---
+++ assets/usePayload-16d12e33.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-c7ade84d.js";const a=e("payload",{state:()=>({payload:{reservation_date:"",start_time:"",end_time:"",last_name:"",first_name:""}})});export{a as u};
--- assets/usePayload-b35cb312.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-9a356d2a.js";const a=e("payload",{state:()=>({payload:{reservation_date:"",start_time:"",end_time:"",last_name:"",first_name:""}})});export{a as u};
